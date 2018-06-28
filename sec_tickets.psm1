d735412b5f44e7b97b76c424a4173a79d76d4774
GalacticMaster <34439944+GalacticMaster@users.noreply.github.com>
2018-06-28 19:15:25 +0530

    Create sec_tickets.psm1

---
+++ sec_tickets.psm1
@@ -0,0 +1,31 @@
+class SecTicket
+{
+    [String] $ticketnumber
+    [String] $pLevel
+    [String] $category
+    [String] $compromise
+    [String] $Activitydate
+    [String] $DDateTime
+    [String] $NDateTime
+    [String] $FireEmail
+    [string] $source
+    [string] $name
+    [string] $filepath
+    [string] $file
+
+ SecTicket ([String]$ticketnumber, [String]$pLevel, [string]$category, [string]$compromise, [string]$Activitydate, [string]$DDateTime, [string]$NDateTime, [string]$FireEmail, [string]$source, [string]$name, [string]$filepath, [string]$file)
+    {
+        $this.ticketnumber = $ticketnumber
+        $this.pLevel = $pLevel
+        $this.category = $category
+        $this.compromise = $compromise
+        $this.Activitydate = $Activitydate
+        $this.DDateTime = $DDateTime
+        $this.NDateTime = $NDateTime
+        $this.FireEmail = $FireEmail
+        $this.source = $source
+        $this.name = $name
+        $this.filepath = $filepath
+        $this.file = $file
+        }
+}
